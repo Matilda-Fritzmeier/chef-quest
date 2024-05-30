562b5ed14f769be27624a6cb2b0e29a0deb5a772
anyelle zanatta <anyelle.ad@gmail.com>
2024-05-30 18:01:21 +0200

    Added dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "dashboard", to: "pages#dashboard"
 
   resources :caterers, except: %i[destroy edit update] do
     resources :bookings, only: %i[new create]
   end
   resources :bookings, only: %i[destroy update]
-  resources :pages, only: %i[dashboard]
 end
